e5bc049deca577ae197667a6325df99d4f8811f4
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-01 13:28:20 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,11 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
   
-  def initialize()
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
   end
   
 end
